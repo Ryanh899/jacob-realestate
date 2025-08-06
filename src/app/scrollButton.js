2015c1fcdf5adbfe8868db18376d1d09d71498e1
ryanh899 <rhazzardphoto@gmail.com>
2025-08-05 18:45:54 -0700

    mobile scroll error

--- src/app/scrollButton.js
+++ src/app/scrollButton.js
@@ -3,27 +3,41 @@
 import React from 'react';
 
 const ScrollButton = () => {
-    const handleScroll = () => {
-        const pageHeight = document.documentElement.scrollHeight;
-        const scrollAmount = pageHeight * 0.23; // 3/4 of the page height
-        window.scrollTo({
-            top: scrollAmount,
-            behavior: 'smooth' // For a smooth scrolling animation
-        });
-    };
+  const handleScroll = () => {
+    const pageHeight = document.documentElement.scrollHeight;
+    const isMobile = window.innerWidth < 768; // Tailwind's md breakpoint
+    const scrollAmount = pageHeight * (isMobile ? 0.19 : 0.23);
 
-    return (
-        <div
-            className="absolute bottom-6 left-1/2 transform -translate-x-1/2 z-30 animate-bounce text-white cursor-pointer"
-            onClick={handleScroll}
-            role="button" // Improve accessibility
-            aria-label="Scroll down" // Improve accessibility
-        >
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 opacity-80" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
-            </svg>
-        </div>
-    );
+    window.scrollTo({
+      top: scrollAmount,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+<div
+  className="absolute bottom-12 md:bottom-6 left-1/2 transform -translate-x-1/2 z-30 animate-bounce text-white cursor-pointer"
+  onClick={handleScroll}
+  role="button"
+  aria-label="Scroll down"
+>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-8 w-8 opacity-80"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M19 9l-7 7-7-7"
+    />
+  </svg>
+</div>
+
+  );
 };
 
 export default ScrollButton;
